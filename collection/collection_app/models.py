e6deb347f934696c44adb303aa9f4b0ccf14fc4e
Rui Constancio <r.miguel.a.c@gmail.com>
2019-04-21 20:32:25 -0300

    First model

--- collection/collection_app/models.py
+++ collection/collection_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Cards(models.Model):
+
+    name = models.CharField('Card Name', max_length=128)
+    edition = models.CharField('Edition', max_length=64)
+    condition = models.CharField('Condition', max_lenghh=2)
+    quantity = models.IntegerField()
+    unit_value = models.FloatField('Single card value')
+    value = models.FloatField('Whole quantity value')
+    location = models.CharField('Position where stored', max_length=128)
+
+    def __str__(self):
+        return f'{self.quantity}x {self.edition}\'s {self.name} in {self.condition} condition, valued in {self.value}'
+
