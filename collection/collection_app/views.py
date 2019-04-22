393b399f73e79ed69774c0852fa7e08d42dc928a
Rui Constancio <r.miguel.a.c@gmail.com>
2019-04-21 22:33:34 -0300

    Make use of render method

--- collection/collection_app/views.py
+++ collection/collection_app/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Cards
 
 def collection(request):
     cards = Cards.objects.all()
-    template = loader.get_template('collection_app/collection.html')
     context = {'cards': cards,}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'collection_app/collection.html', context)
