a671458c0d8d10abc7d507322417f54399c09ab9
Rui Constancio <r.miguel.a.c@gmail.com>
2019-04-21 21:56:53 -0300

    Make fields able to be null

--- collection/collection_app/models.py
+++ collection/collection_app/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Cards(models.Model):
 
     name = models.CharField('Card Name', max_length=128)
-    edition = models.CharField('Edition', max_length=64)
-    condition = models.CharField('Condition', max_length=2)
+    edition = models.CharField('Edition', max_length=64, null=True)
+    condition = models.CharField('Condition', max_length=2, null=True)
     quantity = models.IntegerField()
     unit_value = models.FloatField('Single card value')
     value = models.FloatField('Whole quantity value')
-    location = models.CharField('Position where stored', max_length=128)
+    location = models.CharField('Position where stored', max_length=128, null=True)
 
     def __str__(self):
         return f'{self.quantity}x {self.edition}\'s {self.name} in {self.condition} condition, valued in {self.value}'
