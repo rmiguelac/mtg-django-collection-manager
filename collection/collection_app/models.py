3c66d65c4b8f5ca672a16206d71d32027cff332a
Rui Constancio <r.miguel.a.c@gmail.com>
2019-04-21 21:59:12 -0300

    Fix null fields

--- collection/collection_app/models.py
+++ collection/collection_app/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Cards(models.Model):
 
     name = models.CharField('Card Name', max_length=128)
-    edition = models.CharField('Edition', max_length=64, null=True)
-    condition = models.CharField('Condition', max_length=2, null=True)
+    edition = models.CharField('Edition', max_length=64, blank=True)
+    condition = models.CharField('Condition', max_length=2, blank=True)
     quantity = models.IntegerField()
     unit_value = models.FloatField('Single card value')
     value = models.FloatField('Whole quantity value')
-    location = models.CharField('Position where stored', max_length=128, null=True)
+    location = models.CharField('Position where stored', max_length=128, blank=True)
 
     def __str__(self):
         return f'{self.quantity}x {self.edition}\'s {self.name} in {self.condition} condition, valued in {self.value}'
