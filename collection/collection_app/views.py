113bab4a41bc60d35e54fc4868ad3607ecc9c532
Rui Constancio <r.miguel.a.c@gmail.com>
2019-04-21 22:27:10 -0300

    Add template and info from DB

--- collection/collection_app/views.py
+++ collection/collection_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Cards
 
-
-def index(request):
-    return HttpResponse('This is your collection')
+def collection(request):
+    cards = Cards.objects.all()
+    template = loader.get_template('collection_app/collection.html')
+    context = {'cards': cards,}
+    return HttpResponse(template.render(context, request))
