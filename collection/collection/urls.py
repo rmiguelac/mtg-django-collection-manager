997084d9cf32f9e44a3bde55fb06310c6f2b1e6d
Rui Constancio <r.miguel.a.c@gmail.com>
2019-05-18 13:33:32 -0300

    Clean unecessary url declarations on urls.py

--- collection/collection/urls.py
+++ collection/collection/urls.py
@@ -5,5 +5,4 @@ from django.urls import include, path
 urlpatterns = [
     path(route='admin/', view=admin.site.urls, name='admin'),
     path(route='collection/', view=include('collection_app.urls'), name='collection'),
-    path(route='collection/manage/', view=include('collection_app.urls'), name='manage'),
 ]
