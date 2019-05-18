4a0af5931745facafa862ab2eefe10ad95f41a8e
Rui Constancio <r.miguel.a.c@gmail.com>
2019-05-18 17:56:27 -0300

    Move secret to environment variable

--- collection/collection/settings.py
+++ collection/collection/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '91%q++n9s8(myd%7!9bru942iqjo-1p4c72@kg@5l^f4m2s3*w'
+SECRET_KEY = os.environ['DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
