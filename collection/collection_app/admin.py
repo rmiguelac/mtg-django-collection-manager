bf4564242214eb2e238940f69d93c7eede0439a1
Rui Constancio <r.miguel.a.c@gmail.com>
2019-05-30 23:10:45 -0300

    Structure project to follow best practises

--- collection/collection_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Cards
-
-admin.site.register(Cards)
-
-# Register your models here.
--- collection/collection_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CollectionAppConfig(AppConfig):
-    name = 'collection_app'
